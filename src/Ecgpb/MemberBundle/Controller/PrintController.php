7b16875f43fc487b2c088d8ce4da98ad1376567c
naitsirch <login.naitsirch@arcor.de>
2014-01-02 07:29:24 +0100

    added TCPDF to composer and created new action in PrintController

--- src/Ecgpb/MemberBundle/Controller/PrintController.php
+++ src/Ecgpb/MemberBundle/Controller/PrintController.php
@@ -3,6 +3,8 @@
 namespace Ecgpb\MemberBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Ecgpb\MemberBundle\Controller\PrintController
@@ -28,6 +30,51 @@ class PrintController extends Controller
         return $this->render('EcgpbMemberBundle:Print:html.html.twig', array(
             'date' => new \DateTime(),
             'addresses' => $addresses
+        ));
+    }
+    
+    public function pdfAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $repo = $em->getRepository('EcgpbMemberBundle:Address'); /* @var $repo \Doctrine\Common\Persistence\ObjectRepository */
+        
+        $builder = $repo->createQueryBuilder('address')
+            ->select('address', 'person')
+            ->leftJoin('address.persons', 'person')
+            ->orderBy('address.familyName', 'asc')
+            ->addOrderBy('person.dob', 'asc')
+        ;
+        $addresses = $builder->getQuery()->getResult();
+        
+        $templating = $this->get('templating'); /* @var $templating TwigEngine */
+        
+        $html = $templating->render('EcgpbMemberBundle:Print:pdf.html.twig', array(
+            'date' => new \DateTime(),
+            'addresses' => $addresses,
+        ));
+        
+        $pdf = new \TCPDF('P', 'mm', 'A5', true, 'UTF-8', false);
+        $pdf->SetTitle('ECGPB Member List');
+        $pdf->SetMargins(9, 9, 9);
+        $pdf->SetPrintHeader(false);
+        $pdf->SetPrintFooter(false);
+        $pdf->SetAutoPageBreak(true, 9);
+
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        $pdf->SetFont('dejavusans', '', 10);
+
+        $pdf->AddPage();
+
+        $pdf->writeHTML($html, true, false, true, false, '');
+
+        $content = $pdf->Output('ECGPB Member List.pdf', 'S');
+        
+        return new Response($content, 200, array(
+            'Content-Type' => 'application/octet-stream',
+            'Content-Disposition' => 'attachment; filename="ECGPB Member List.pdf"',
         ));
     }
 }
