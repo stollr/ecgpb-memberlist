2f4f146bb575a45acdefd3570c7b8f5565ccafff
naitsirch <login.naitsirch@arcor.de>
2014-04-14 21:28:43 +0200

    Catching exception in MinistryCategoryController->updateAction

--- src/Ecgpb/MemberBundle/Controller/MinistryCategoryController.php
+++ src/Ecgpb/MemberBundle/Controller/MinistryCategoryController.php
@@ -4,6 +4,7 @@ namespace Ecgpb\MemberBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use JMS\Serializer\SerializationContext;
@@ -56,57 +57,61 @@ class MinistryCategoryController extends Controller
             throw new \InvalidArgumentException('Wrong content type provided. JSON is expected.');
         }
 
-        $em = $this->getDoctrine()->getManager();
-        $clientMinistryCategories = json_decode($request->getContent(), true);
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $clientMinistryCategories = json_decode($request->getContent(), true);
 
-        $categories = $em->getRepository('EcgpbMemberBundle:Ministry\Category')->findAll();
-        /* @var $categories Category[] */
+            $categories = $em->getRepository('EcgpbMemberBundle:Ministry\Category')->findAll();
+            /* @var $categories Category[] */
 
-        // get all ids of already existing categories
-        $existingCategoryIds = array();
-        foreach ($clientMinistryCategories as $clientMinistryCategory) {
-            if (!empty($clientMinistryCategory['id'])) {
-                $existingCategoryIds[] = $clientMinistryCategory['id'];
+            // get all ids of already existing categories
+            $existingCategoryIds = array();
+            foreach ($clientMinistryCategories as $clientMinistryCategory) {
+                if (!empty($clientMinistryCategory['id'])) {
+                    $existingCategoryIds[] = $clientMinistryCategory['id'];
+                }
             }
+
+            // delete obsolete entities
+            foreach ($categories as $category) {
+                if (!in_array($category->getId(), $existingCategoryIds)) {
+                    $em->remove($category);
+                }
+            }
+
+            // create and update entities
+            foreach ($clientMinistryCategories as $clientMinistryCategory) {
+                if (empty($clientMinistryCategory['id'])) {
+                    $category = new Category();
+                    $categories[] = $category;
+                } else {
+                    $filtered = array_filter($categories, function($category) use ($clientMinistryCategory) {
+                        return $category->getId() == $clientMinistryCategory['id'];
+                    });
+                    $category = reset($filtered);
+                    $existingCategoryIds[] = $clientMinistryCategory['id'];
+                }
+                $form = $this->createForm(new CategoryType(), $category, array(
+                    'csrf_protection' => false,
+                ));
+                $form->submit($clientMinistryCategory);
+
+                if ($form->isValid()) {
+                    $em->persist($category);
+                } else {
+                    return new Response('Invalid entity', 400, array('Content-Type' => 'application/json'));
+                }
+            }
+
+            $em->flush();
+
+            // response
+            $serializer = $this->get('jms_serializer');
+            $categoriesJson = $serializer->serialize($categories, 'json', SerializationContext::create()->setGroups(array('MinistryCategoryListing')));
+
+            return new Response($categoriesJson, 200, array('Content-Type' => 'application/json'));
+        } catch (\Exception $e) {
+            return new JsonResponse($e->getMessage(), 401);
         }
-
-        // delete obsolete entities
-        foreach ($categories as $category) {
-            if (!in_array($category->getId(), $existingCategoryIds)) {
-                $em->remove($category);
-            }
-        }
-
-        // create and update entities
-        foreach ($clientMinistryCategories as $clientMinistryCategory) {
-            if (empty($clientMinistryCategory['id'])) {
-                $category = new Category();
-                $categories[] = $category;
-            } else {
-                $filtered = array_filter($categories, function($category) use ($clientMinistryCategory) {
-                    return $category->getId() == $clientMinistryCategory['id'];
-                });
-                $category = reset($filtered);
-                $existingCategoryIds[] = $clientMinistryCategory['id'];
-            }
-            $form = $this->createForm(new CategoryType(), $category, array(
-                'csrf_protection' => false,
-            ));
-            $form->submit($clientMinistryCategory);
-
-            if ($form->isValid()) {
-                $em->persist($category);
-            } else {
-                return new Response('Invalid entity', 400, array('Content-Type' => 'application/json'));
-            }
-        }
-
-        $em->flush();
-
-        // response
-        $serializer = $this->get('jms_serializer');
-        $categoriesJson = $serializer->serialize($categories, 'json', SerializationContext::create()->setGroups(array('MinistryCategoryListing')));
-        
-        return new Response($categoriesJson, 200, array('Content-Type' => 'application/json'));
     }
 }
