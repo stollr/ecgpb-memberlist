997e9969b50ed29398ccaa9449fb3058eb51abba
naitsirch <naitsirch@e.mail.de>
2019-11-19 21:10:16 +0100

    Symfony Upgrade to 4.3

--- web/app.php
+++ web/app.php
@@ -5,11 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 umask(0000);
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
