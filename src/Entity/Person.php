f7144028ba7cfd068a0617623ac1e2b806cf491f
naitsirch <naitsirch@e.mail.de>
2022-01-07 22:50:55 +0100

    Fixed namespace of exception class

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -455,7 +455,7 @@ class Person
     public function setWorkingGroup(\App\Entity\WorkingGroup $workingGroup = null)
     {
         if ($this->gender && $workingGroup && $this->gender !== $workingGroup->getGender()) {
-            throw new InvalidArgumentException('This person is not compatible to the passed working group.');
+            throw new \InvalidArgumentException('This person is not compatible to the passed working group.');
         }
 
         $this->workingGroup = $workingGroup;
