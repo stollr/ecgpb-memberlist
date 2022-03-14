c71d141c6fb1959c91bfa019f9927f5400458b90
naitsirch <naitsirch@e.mail.de>
2022-03-14 23:53:45 +0100

    Fixed line breaks in PDF texts

--- src/PdfGenerator/Generator.php
+++ src/PdfGenerator/Generator.php
@@ -13,9 +13,10 @@ abstract class Generator
 {
     const FONT_SIZE_XL = 11.5;
     const FONT_SIZE_L = 10;
-    const FONT_SIZE_M = 8.5;
-    const FONT_SIZE_S = 7.5;
-    const FONT_SIZE_XS = 6.5;
+    const FONT_SIZE_M = 9.25;
+    const FONT_SIZE_S = 8.5;
+    const FONT_SIZE_XS = 7.5;
+    const FONT_SIZE_XXS = 6.5;
 
     public function addHeadlineMargin(\TCPDF $pdf)
     {
@@ -24,7 +25,13 @@ abstract class Generator
 
     public function addParagraphMargin(\TCPDF $pdf)
     {
-        $pdf->SetY($pdf->GetY() + 10);
+        if (bccomp($pdf->getFontSizePt(), self::FONT_SIZE_M, 2) === 0) {
+            $pdf->SetY($pdf->GetY() + 6);
+        } elseif (bccomp($pdf->getFontSizePt(), self::FONT_SIZE_S, 2) === 0) {
+            $pdf->SetY($pdf->GetY() + 4);
+        } else {
+            $pdf->SetY($pdf->GetY() + 10);
+        }
     }
 
     public function useFontSizeXL(\TCPDF $pdf)
@@ -77,19 +84,25 @@ abstract class Generator
         $lineWidth = $x - $margins['left'];
         
         for ($c = 0; $c < $strlen; $c++) {
-            if ($lineWidth + $charWidths[$c] > $maxLineWidth || "\n" == $text[$c]) {
+            $glyph = mb_substr($text, $c, 1, 'UTF-8');
+
+            if ($lineWidth + $charWidths[$c] > $maxLineWidth || "\n" === $glyph) {
                 $pdf->Text($x, $y, $lineText, false, false, true, 0, 1);
                 $x = $pdf->GetX();
                 $y = $pdf->GetY();
                 $lineText = '';
                 $lineWidth = 0;
+
+                if ("\n" === $glyph) {
+                    $c++;
+                }
                 
                 // remove white spaces at the beginning of the next line
-                while (' ' == mb_substr($text, $c, 1, 'UTF-8')) {
+                while (' ' === ($glyph = mb_substr($text, $c, 1, 'UTF-8'))) {
                     $c++;
                 }
             }
-            $lineText .= mb_substr($text, $c, 1, 'UTF-8');
+            $lineText .= $glyph;
             $lineWidth += $charWidths[$c];
         }
         
@@ -101,5 +114,11 @@ abstract class Generator
     public function addTable(\TCPDF $pdf)
     {
         return new Table($pdf);
+    }
+
+    protected function getWordWiths($text)
+    {
+        $words = preg_split('/\s|\-', $text, -1, PREG_SPLIT_NO_EMPTY);
+        $charWidths = $pdf->GetStringWidth($text, '', $pdf->getFontStyle(), $pdf->getFontSize(), true);
     }
 }
