a22748b73181849e07c5983e5d993462fc3f3a3b
Christian Stoller <christian.stoller@mail.de>
2026-02-04 22:29:31 +0100

    Added doctrine 'array' type to support Gedmo Loggable extension with DBAL 4

---
+++ src/Doctrine/Types/ArrayType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Doctrine\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+use function is_resource;
+use function restore_error_handler;
+use function serialize;
+use function set_error_handler;
+use function stream_get_contents;
+use function unserialize;
+
+/**
+ * Type that maps a PHP array to a clob SQL type.
+ */
+class ArrayType extends Type
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getClobTypeDeclarationSQL($column);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): ?string
+    {
+        // @todo 3.0 - $value === null check to save real NULL in database
+        return serialize($value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): mixed
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $value = is_resource($value) ? stream_get_contents($value) : $value;
+
+        set_error_handler(function (int $code, string $message): bool {
+            throw ConversionException::conversionFailedUnserialization($this->getName(), $message);
+        });
+
+        try {
+            return unserialize($value);
+        } finally {
+            restore_error_handler();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'array';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+}
